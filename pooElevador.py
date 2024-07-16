7a03d3d65cc0566a8d35f317ef81e1d2c492bc63
David Portela <portela.davidr@gmail.com>
2024-07-16 20:25:33 -0300

    Create pooElevador.py

---
+++ pooElevador.py
@@ -0,0 +1,48 @@
+class Elevador:
+    def __init__(self, totalCapacidade, totalAndar):
+        self.totalCapacidade = totalCapacidade
+        self.atualCapacidade = 0
+        self.totalAndar = totalAndar
+        self.atualAndar = 0
+
+    def subir(self):
+        if self.atualAndar < self.totalAndar:
+            self.atualAndar += 1
+            print("Subindo...")
+        else:
+            print("VOCÊ ESTÁ NO ANDAR MAIS ALTO!")
+
+    def descer(self):
+        if self.atualAndar > 0:
+            self.atualAndar -= 1
+            print("Descendo...")
+        else:
+            print("VOCÊ JÁ ESTÁ NO TÉRREO!")
+
+    def entrar(self):
+        if self.atualCapacidade < self.totalCapacidade:
+            self.atualCapacidade += 1
+            print("Entrando uma pessoa...")
+        else:
+            print("O ELEVADOR ESTÁ CHEIO!")
+
+    def sair(self):
+        if self.atualCapacidade > 0:
+            self.atualCapacidade -= 1
+            print("Saindo uma pessoa...")
+        else:
+            print("NÃO TEM NINGUÉM NO ELEVADOR!")
+
+# Teste da classe
+elevador = Elevador(5, 10)
+
+elevador.entrar()
+elevador.entrar()
+elevador.subir()
+elevador.entrar()
+elevador.subir()
+elevador.sair()
+elevador.descer()
+elevador.sair()
+elevador.sair()
+elevador.descer()
